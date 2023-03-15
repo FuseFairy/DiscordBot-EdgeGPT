e1d046b2ff4b9d51793483eb6823386ebf808bc1
Danny <dd8611706@gmail.com>
2023-03-16 03:11:32 +0800

    feat: can switch conversation style

--- cogs/edgegpt.py
+++ cogs/edgegpt.py
@@ -1,6 +1,6 @@
 import discord
 import asyncio
-from EdgeGPT import Chatbot
+from EdgeGPT import Chatbot, ConversationStyle
 from discord.ext import commands
 from core.classes import Cog_Extension
 from src import log
@@ -10,16 +10,26 @@ load_dotenv()
 
 bot = commands.Bot(command_prefix='!', intents = discord.Intents.all())
 chatbot = Chatbot("./cookies.json")
-
 logger = log.setup_logger(__name__)
-
 sem = asyncio.Semaphore(1)
+# default conversation style
+conversation_style = "balanced"
+
+# switch conversation style
+async def switch_style(style: str):
+    global conversation_style
+    conversation_style = style
 
 async def send_message(message, user_message):
     async with sem:
         try:
             ask = f"> **{user_message}** - <@{str(message.user.id)}> \n\n"
-            temp = await chatbot.ask(user_message)
+            if conversation_style == "creative":
+                temp = await chatbot.ask(prompt=user_message, conversation_style=ConversationStyle.creative)
+            elif conversation_style == "precise":
+                temp = await chatbot.ask(prompt=user_message, conversation_style=ConversationStyle.precise)
+            else:
+                temp = await chatbot.ask(prompt=user_message, conversation_style=ConversationStyle.balanced)
             try:
                 text = temp["item"]["messages"][1]["text"]
             except:
@@ -54,10 +64,11 @@ class edgegpt(Cog_Extension):
         username = str(interaction.user)
         usermessage = message
         channel = str(interaction.channel)
-        logger.info(f"\x1b[31m{username}\x1b[0m : '{usermessage}' ({channel}) [EdgeGPT]")
+        logger.info(f"\x1b[31m{username}\x1b[0m : '{usermessage}' ({channel}) [Style: {conversation_style}]")
         task = asyncio.create_task(send_message(interaction, usermessage))
         await asyncio.gather(task)
 
+    # reset Bing conversation history
     @bot.tree.command(name="reset_bing", description="Complete reset Bing conversation history")
     async def resetbing(self, interaction: discord.Interaction):
         open('discord_bot.log', 'w').close()
@@ -65,6 +76,30 @@ class edgegpt(Cog_Extension):
         await chatbot.reset()
         await interaction.followup.send("> **Info: Reset finish.**")
         logger.warning("\x1b[31mBing has been successfully reset\x1b[0m")
+
+    # switch conversation style to creative
+    @bot.tree.command(name="style_creative", description="Switch conversation style to creative")
+    async def switch_style_creative(self, interaction: discord.Interaction):
+        await interaction.response.defer(ephemeral=False)
+        await switch_style("creative")
+        await interaction.followup.send("> **Info: successfull switch conversation style to creative.**")
+        logger.warning("\x1b[31mConversation style has been successfully switch to creative\x1b[0m")
+
+    # switch conversation style to balanced
+    @bot.tree.command(name="style_balanced", description="Switch conversation style to balanced")
+    async def switch_style_balanced(self, interaction: discord.Interaction):
+        await interaction.response.defer(ephemeral=False)
+        await switch_style("balanced")
+        await interaction.followup.send("> **Info: successfull switch conversation style to balanced.**")
+        logger.warning("\x1b[31mConversation style has been successfully switch to balanced\x1b[0m")
+
+    # switch conversation style to precise
+    @bot.tree.command(name="style_precise", description="Switch conversation style to precise")
+    async def switch_style_precise(self, interaction: discord.Interaction):
+        await interaction.response.defer(ephemeral=False)
+        await switch_style("precise")
+        await interaction.followup.send("> **Info: successfull switch conversation style to precise.**")
+        logger.warning("\x1b[31mConversation style has been successfully switch to precise\x1b[0m")
 
 async def setup(bot):
     await bot.add_cog(edgegpt(bot))

664fbe5fb57f79f0be8b0ff59371482d3efcd6bd
Error404 <78064680+FuseFairy@users.noreply.github.com>
2023-03-23 20:37:44 +0800

    Revert "refactor: delete check_verion()"

--- bot.py
+++ bot.py
@@ -1,15 +1,34 @@
 import discord
 import os
 import src.log
+import sys
+import pkg_resources
 from discord.ext import commands
 from dotenv import load_dotenv
 
-load_dotenv(dotenv_path="./.env")
+load_dotenv()
 
 bot = commands.Bot(command_prefix='!', intents = discord.Intents.all())
 
 # init loggger
 logger = src.log.setup_logger(__name__)
+
+def check_verion() -> None:
+    # Read the requirements.txt file and add each line to a list
+    with open('requirements.txt') as f:
+        required = f.read().splitlines()
+
+    # For each library listed in requirements.txt, check if the corresponding version is installed
+    for package in required:
+        # Use the pkg_resources library to get information about the installed version of the library
+        package_name, package_verion = package.split('==')
+        installed = pkg_resources.get_distribution(package_name)
+        # Extract the library name and version number
+        name, version = installed.project_name, installed.version
+        # Compare the version number to see if it matches the one in requirements.txt
+        if package != f'{name}=={version}':
+            logger.error(f'{name} version {version} is installed but does not match the requirements')
+            sys.exit()
 
 @bot.event
 async def on_ready():
@@ -45,4 +64,5 @@ async def clean(ctx):
     await ctx.send(f'Has been emptied')
 
 if __name__ == '__main__':
+    check_verion()
     bot.run(os.getenv("DISCORD_BOT_TOKEN"))

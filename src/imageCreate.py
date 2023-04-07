31f64b5a2e48e5e871df712ad3446b2c1ff6bb6e
Error404 <78064680+FuseFairy@users.noreply.github.com>
2023-04-07 19:13:42 +0800

    feat: change to send messages using embed

--- src/imageCreate.py
+++ src/imageCreate.py
@@ -1,6 +1,5 @@
 import discord
 import json
-import os
 import asyncio
 from BingImageCreator import ImageGenAsync
 from src import log
@@ -17,23 +16,16 @@ image_generator = ImageGenAsync(auth_cookie, True)
 logger = log.setup_logger(__name__)
 sem = asyncio.Semaphore(1)
 
-try:
-    os.mkdir("images")
-except:
-    pass
-
 async def create_image(interaction: discord.Interaction, prompt: str):
     async with sem:
         try:
-            files = []
-            # Delete existing images
-            [os.remove(f"./images/{image}") for image in os.listdir("./images")]
+            embeds = []
             prompts = f"> **{prompt}** - <@{str(interaction.user.id)}> (***BingImageCreator***)\n\n"
-            # Fetches image links and download images
-            await image_generator.save_images(await image_generator.get_images(prompt), "./images")
-            # Add images to list of files
-            [files.append(discord.File(f"./images/{image}")) for image in os.listdir("./images")]
-            await interaction.followup.send(prompts, files=files, wait=True)
+            # Fetches image links 
+            images = await image_generator.get_images(prompt)
+            # Add embed to list of embeds
+            [embeds.append(discord.Embed(url="https://www.bing.com/").set_image(url=image_link)) for image_link in images]
+            await interaction.followup.send(prompts, embeds=embeds, wait=True)
         except asyncio.TimeoutError:
             await interaction.followup.send("> **Error: Request timed out.**")
             logger.exception("Error while create image: Request timed out.")

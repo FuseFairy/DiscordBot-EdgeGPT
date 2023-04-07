59cb2b8f70b00c3d8cf0edd4393b1684046c1572
Error404 <78064680+FuseFairy@users.noreply.github.com>
2023-04-07 10:11:31 +0800

    fix: solve timeout error

--- src/imageCreate.py
+++ src/imageCreate.py
@@ -7,14 +7,16 @@ from src import log
 
 with open("./cookies.json", encoding="utf-8") as file:
     cookie_json = json.load(file)
-    for cookie in cookie_json:
-        if cookie.get("name") == "_U":
-            auth_cookie = cookie.get("value")
-            break
+
+for cookie in cookie_json:
+    if cookie.get("name") == "_U":
+        auth_cookie = cookie.get("value")
+        break
 
 image_generator = ImageGenAsync(auth_cookie, True)
 logger = log.setup_logger(__name__)
 sem = asyncio.Semaphore(1)
+
 try:
     os.mkdir("images")
 except:
@@ -24,16 +26,20 @@ async def create_image(interaction: discord.Interaction, prompt: str):
     async with sem:
         try:
             files = []
-            # Delete image from file
+            # Delete existing images
             [os.remove(f"./images/{image}") for image in os.listdir("./images")]
             prompts = f"> **{prompt}** - <@{str(interaction.user.id)}> (***BingImageCreator***)\n\n"
             # Fetches image links and download images
             await image_generator.save_images(await image_generator.get_images(prompt), "./images")
-            # Add image to list
+            # Add images to list of files
             [files.append(discord.File(f"./images/{image}")) for image in os.listdir("./images")]
-            await interaction.followup.send(prompts, files=files)
-            await asyncio.sleep(15)
+            await interaction.followup.send(prompts, files=files, wait=True)
+        except asyncio.TimeoutError:
+            await interaction.followup.send("> **Error: Request timed out.**", ephemeral=True)
+            logger.exception("Error while create image: Request timed out.")
         except Exception as e:
-            await interaction.followup.send(f"> **{e}**")
+            await interaction.followup.send(f"> **Error: {e}**", ephemeral=True)
             logger.exception(f"Error while create image: {e}")
+        finally:
+            # Wait for 15 seconds before processing the next request
             await asyncio.sleep(15)

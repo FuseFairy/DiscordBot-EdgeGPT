35bd3afc22cbd8426ba8f8d3bbd05d149e3cdb01
Error404 <78064680+FuseFairy@users.noreply.github.com>
2023-04-19 06:57:32 +0800

    feat: modify if/else

--- bot.py
+++ bot.py
@@ -95,8 +95,8 @@ async def upload(ctx):
                 await ctx.send(f'Upload new cookies successfully!')
                 logger.warning("\x1b[31mCookies has been setup successfully\x1b[0m")
                 os.system("python bot.py")
-                else:
-                    await ctx.send("Didn't get any txt file.")
+             else:
+                await ctx.send("Didn't get any txt file.")
     else:
         await ctx.send("Didn't get any file.")
 

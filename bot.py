67294e6d9923517a704ca1eb4e25a7cf4686262d
Danny <dd8611706@gmail.com>
2023-04-19 05:47:28 +0800

    feat: add new prefix command

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import os
 import src.log
 import sys
 import pkg_resources
+import json
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -32,36 +33,75 @@ def check_verion() -> None:
 
 @bot.event
 async def on_ready():
-    logger.info(f'{bot.user} is now running!')
+    bot_status = discord.Status.online
+    bot_activity = discord.Activity(type=discord.ActivityType.playing, name = "bing.com")
+    await bot.change_presence(status = bot_status, activity = bot_activity)
     for Filename in os.listdir('./cogs'):
         if Filename.endswith('.py'):
-            await bot.load_extension(f'cogs.{Filename[:-3]}')  
+            await bot.load_extension(f'cogs.{Filename[:-3]}')
+    logger.info(f'{bot.user} is now running!')
     print("Bot is Up and Ready!")
     try:
         synced = await bot.tree.sync()
         print(f"Synced {len(synced)} commands")
     except Exception as e:
         print(e)
-        
+
+# Load command
+@commands.is_owner()   
 @bot.command()
 async def load(ctx, extension):
     await bot.load_extension(f'cogs.{extension}')
     await ctx.send(f'Loaded {extension} done.')
 
+# Unload command
+@commands.is_owner()
 @bot.command()
 async def unload(ctx, extension):
     await bot.unload_extension(f'cogs.{extension}')
     await ctx.send(f'Un-Loaded {extension} done.')
 
+# Reload command
+@commands.is_owner()
 @bot.command()
 async def reload(ctx, extension):
     await bot.reload_extension(f'cogs.{extension}')
     await ctx.send(f'Re-Loaded {extension} done.')
 
+# Empty discord_bot.log file
+@commands.is_owner()
 @bot.command()
 async def clean(ctx):
     open('discord_bot.log', 'w').close()
-    await ctx.send(f'Has been emptied')
+    await ctx.send(f'Successfully emptied the file!')
+
+# Get discord_bot.log file
+@commands.is_owner()
+@bot.command()
+async def getLog(ctx):
+    try:
+        with open('discord_bot.log', 'rb') as f:
+            file = discord.File(f)
+        await ctx.author.send(file=file)
+        await ctx.send("Send successfully!")
+    except:
+        await ctx.send("Send failed!")
+
+# Upload new Bing cookies than restart the bot
+@commands.is_owner()
+@bot.command()
+async def upload(ctx):
+    if ctx.message.attachments:
+        for attachment in ctx.message.attachments:
+            if str(attachment)[-4:] == ".txt":
+                content = await attachment.read()
+                with open("cookies.json", "w", encoding = "utf-8") as f:
+                    json.dump(json.loads(content), f, indent = 2)
+                if not isinstance(ctx.channel, discord.abc.PrivateChannel):
+                    await ctx.message.delete()
+                await ctx.send(f'Upload new cookies successfully!')
+                logger.warning("\x1b[31mCookies has been setup successfully\x1b[0m")
+                os.system("python bot.py")
 
 if __name__ == '__main__':
     check_verion()

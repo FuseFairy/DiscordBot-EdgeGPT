b301fb38601ae3807d6e6fa01bf3679c51744d2f
Danny <dd8611706@gmail.com>
2023-04-24 23:12:44 +0800

    feat: conversation can generate image

--- src/response.py
+++ src/response.py
@@ -1,5 +1,6 @@
 import discord
 import asyncio
+import re
 from EdgeGPT import Chatbot, ConversationStyle
 from src import log
 from config import load_config
@@ -9,17 +10,17 @@ logger = log.setup_logger(__name__)
 USE_SUGGEST_RESPONSES = load_config.config["USE_SUGGEST_RESPONSES"]
 sem = asyncio.Semaphore(1)
 
-# to add suggest responses
+# To add suggest responses
 class MyView(discord.ui.View):
     def __init__(self, chatbot: Chatbot, conversation_style:str, suggest_responses:list):
         super().__init__(timeout=120)
-        # add buttons
+        # Add buttons
         for label in suggest_responses:
             button = discord.ui.Button(label=label)
-            # button event
+            # Button event
             async def callback(interaction: discord.Interaction, button: discord.ui.Button):
                 await interaction.response.defer(ephemeral=False, thinking=True)
-                # when click the button, all buttons will disable.
+                # When click the button, all buttons will disable.
                 for child in self.children:
                     child.disabled = True
                 await interaction.followup.edit_message(message_id=interaction.message.id, view=self)
@@ -39,59 +40,81 @@ async def send_message(chatbot: Chatbot, message: discord.Interaction, user_mess
         superscript_map = {'0': '⁰', '1': '¹', '2': '²', '3': '³', '4': '⁴', '5': '⁵', '6': '⁶', '7': '⁷', '8': '⁸', '9': '⁹'}
         reply = ''
         text = ''
-        embed = ''
+        link_embed = ''
+        images_embed = []
         all_url = []
         try:
-            # change conversation style
+            # Change conversation style
             if conversation_style == "creative":
                 reply = await chatbot.ask(prompt=user_message, conversation_style=ConversationStyle.creative, wss_link="wss://sydney.bing.com/sydney/ChatHub")
             elif conversation_style == "precise":
                 reply = await chatbot.ask(prompt=user_message, conversation_style=ConversationStyle.precise, wss_link="wss://sydney.bing.com/sydney/ChatHub")
             else:
                 reply = await chatbot.ask(prompt=user_message, conversation_style=ConversationStyle.balanced, wss_link="wss://sydney.bing.com/sydney/ChatHub")
-            # get reply text
-            text = f"{reply['item']['messages'][1]['text']}"
+            # Get reply text
+            try:
+                text = f"{reply['item']['messages'][4]['text']}"
+            except:
+                text = f"{reply['item']['messages'][1]['text']}"
             text = re.sub(r'\[\^(\d+)\^\]', lambda match: ''.join(superscript_map.get(digit, digit) for digit in match.group(1)), text)
             # Get the URL, if available
-            if len(reply['item']['messages'][1]['sourceAttributions']) != 0:
-                for i, url in enumerate(reply['item']['messages'][1]['sourceAttributions'], start=1):
-                    if len(url['providerDisplayName']) == 0:
-                        all_url.append(f"{i}. [{url['seeMoreUrl']}]({url['seeMoreUrl']})")
-                    else:
-                        all_url.append(f"{i}. [{url['providerDisplayName']}]({url['seeMoreUrl']})")
+            try:
+                for search_results in reply["item"]["messages"][2]["groundingInfo"]:
+                    for url in reply["item"]["messages"][2]["groundingInfo"][search_results]:
+                        if url['title'] != None:
+                            all_url.append(f"{url['index']}. [{url['title']}]({url['url']})")
+                        else:
+                            all_url.append(f"{url['index']}. [{url['url']}]({url['url']})")
                 link_text = "\n".join(all_url)
-                embed = discord.Embed(description=link_text)
+                link_embed = discord.Embed(description=link_text)
+            except:
+                pass
             # Set the final message
             user_message = user_message.replace("\n", "")
             ask = f"> **{user_message}** - <@{str(message.user.id)}> (***style: {conversation_style}***)\n\n"
             response = f"{ask}{text}"
-            # discord limit about 2000 characters for a message
+            # Discord limit about 2000 characters for a message
             while len(response) > 2000:
                 temp = response[:2000]
                 response = response[2000:]
                 await message.followup.send(temp)
-            # add all suggest responses in list
+            # Get the image, if available
+            try:
+                all_image = re.findall("https?://[\w\./]+", str(reply["item"]["messages"][1]["adaptiveCards"][0]["body"][0]["text"]))
+                [images_embed.append(discord.Embed(url="https://www.bing.com/").set_image(url=image_link)) for image_link in all_image]
+            except:
+                pass
+            # Add all suggest responses in list
             if USE_SUGGEST_RESPONSES:
                 suggest_responses = []
-                for suggest in reply["item"]["messages"][1]["suggestedResponses"]:
-                    suggest_responses.append(suggest["text"])
-                if embed:
-                    await message.followup.send(response, view=MyView(chatbot, conversation_style, suggest_responses), embeds=[embed])
+                try:
+                    for suggest in reply["item"]["messages"]:
+                        try:
+                            for suggest_message in suggest["suggestedResponses"]:
+                                suggest_responses.append(suggest_message["text"])
+                        except:
+                            pass
+                except:
+                    pass
+                if images_embed:
+                    await message.followup.send(response, view=MyView(chatbot, conversation_style, suggest_responses), embeds=images_embed, wait=True)                
+                elif link_embed:
+                    await message.followup.send(response, view=MyView(chatbot, conversation_style, suggest_responses), embed=link_embed, wait=True)
                 else:
-                    await message.followup.send(response, view=MyView(chatbot, conversation_style, suggest_responses))
+                    await message.followup.send(response, view=MyView(chatbot, conversation_style, suggest_responses), wait=True)
             else:
-                if embed:
-                    await message.followup.send(response, embeds=[embed])
+                if images_embed:
+                    await message.followup.send(response, embeds=images_embed, wait=True)
+                elif link_embed:
+                    await message.followup.send(response, embed=link_embed, wait=True)
                 else:
-                    await message.followup.send(response)
+                    await message.followup.send(response, wait=True)
         except Exception as e:
                 print(reply)
-                if reply["item"]["throttling"]["numUserMessagesInConversation"] and reply["item"]["throttling"]["numUserMessagesInConversation"] > reply["item"]["throttling"]["maxNumUserMessagesInConversation"]:
-                    await message.followup.send("> **Oops, I think we've reached the end of this conversation. Please reset the bot!**")
-                    logger.exception(f"Error while sending message: The maximum number of conversations in a round has been reached")
-                elif reply["item"]["result"]["value"] and reply["item"]["result"]["value"] == "Throttled":
-                    await message.followup.send("> **Error: We're sorry, but you've reached the maximum number of messages you can send to Bing in a 24-hour period. Check back later!**")
-                    logger.exception("Error while sending message: The daily conversation limit has been reached")
-                else:
-                    await message.followup.send("> **Please try again later or reset bot**")
+                try:
+                    if reply["item"]["throttling"]["numUserMessagesInConversation"] and reply["item"]["throttling"]["numUserMessagesInConversation"] > reply["item"]["throttling"]["maxNumUserMessagesInConversation"]:
+                        await message.followup.send("> **Oops, I think we've reached the end of this conversation. Please reset the bot!**")
+                        logger.exception(f"Error while sending message: The maximum number of conversations in a round has been reached")
+                except:
+                    await message.followup.send("> **Error: Please try again later or reset bot**")
                     logger.exception(f"Error while sending message: {e}")

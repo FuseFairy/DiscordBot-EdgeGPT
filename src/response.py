910bf636955a370ee2680114138d64527e335962
Danny <dd8611706@gmail.com>
2023-04-26 02:45:34 +0800

    feat: make button only for command user

--- src/response.py
+++ src/response.py
@@ -1,5 +1,4 @@
 import discord
-import asyncio
 import re
 from EdgeGPT import Chatbot, ConversationStyle
 from src import log
@@ -8,35 +7,46 @@ from functools import partial
 
 logger = log.setup_logger(__name__)
 USE_SUGGEST_RESPONSES = load_config.config["USE_SUGGEST_RESPONSES"]
-sem = asyncio.Semaphore(1)
+using_func = {}
 
 # To add suggest responses
 class MyView(discord.ui.View):
-    def __init__(self, chatbot: Chatbot, conversation_style:str, suggest_responses:list):
+    def __init__(self,interaction: discord.Interaction, chatbot: Chatbot, conversation_style:str, suggest_responses:list):
         super().__init__(timeout=120)
+        self.button_author =interaction.user.id
         # Add buttons
         for label in suggest_responses:
             button = discord.ui.Button(label=label)
             # Button event
-            async def callback(interaction: discord.Interaction, button: discord.ui.Button):
-                await interaction.response.defer(ephemeral=False, thinking=True)
-                # When click the button, all buttons will disable.
-                for child in self.children:
-                    child.disabled = True
-                await interaction.followup.edit_message(message_id=interaction.message.id, view=self)
-                username = str(interaction.user)
-                usermessage = button.label
-                channel = str(interaction.channel)
-                logger.info(f"\x1b[31m{username}\x1b[0m : '{usermessage}' ({channel}) [Style: {conversation_style}] [button]")
-                task = asyncio.create_task(send_message(chatbot, interaction, usermessage, conversation_style))
-                await asyncio.gather(task)
+            async def callback(interaction: discord.Interaction, button_author: int):
+                if interaction.user.id != button_author:
+                    await interaction.response.defer(ephemeral=True, thinking=True)
+                    await interaction.followup.send("You don't have permission to press this button.")
+                elif not using_func[interaction.user.id]:
+                    await interaction.response.defer(ephemeral=False, thinking=True)
+                    # When click the button, all buttons will disable.
+                    for child in self.children:
+                        child.disabled = True
+                    await interaction.followup.edit_message(message_id=interaction.message.id, view=self)
+                    username = str(interaction.user)
+                    usermessage = button.label
+                    channel = str(interaction.channel)
+                    logger.info(f"\x1b[31m{username}\x1b[0m : '{usermessage}' ({channel}) [Style: {conversation_style}] [button]")
+                    await send_message(chatbot, interaction, usermessage, conversation_style)
+                else:
+                    await interaction.response.defer(ephemeral=True, thinking=True)
+                    await interaction.followup.send("Please wait for your last conversation to finish.")
             self.add_item(button)
-            self.children[-1].callback = partial(callback, button=button)
+            self.children[-1].callback = partial(callback, button_author=self.button_author)
 
+async def get_func_status(user_id):
+    return using_func[user_id]
 
-async def send_message(chatbot: Chatbot, message: discord.Interaction, user_message: str, conversation_style: str):
-    async with sem:
-        import re
+async def set_func_status(user_id, status: bool):
+    using_func[user_id] = status
+
+async def send_message(chatbot: Chatbot, interaction: discord.Interaction, user_message: str, conversation_style: str):
+        using_func[interaction.user.id] = True
         superscript_map = {'0': '⁰', '1': '¹', '2': '²', '3': '³', '4': '⁴', '5': '⁵', '6': '⁶', '7': '⁷', '8': '⁸', '9': '⁹'}
         reply = ''
         text = ''
@@ -71,13 +81,13 @@ async def send_message(chatbot: Chatbot, message: discord.Interaction, user_mess
                 pass
             # Set the final message
             user_message = user_message.replace("\n", "")
-            ask = f"> **{user_message}** - <@{str(message.user.id)}> (***style: {conversation_style}***)\n\n"
+            ask = f"> **{user_message}** - <@{str(interaction.user.id)}> (***style: {conversation_style}***)\n\n"
             response = f"{ask}{text}"
             # Discord limit about 2000 characters for a message
             while len(response) > 2000:
                 temp = response[:2000]
                 response = response[2000:]
-                await message.followup.send(temp)
+                await interaction.followup.send(temp)
             # Get the image, if available
             try:
                 all_image = re.findall("https?://[\w\./]+", str(reply["item"]["messages"][1]["adaptiveCards"][0]["body"][0]["text"]))
@@ -97,24 +107,25 @@ async def send_message(chatbot: Chatbot, message: discord.Interaction, user_mess
                 except:
                     pass
                 if images_embed:
-                    await message.followup.send(response, view=MyView(chatbot, conversation_style, suggest_responses), embeds=images_embed, wait=True)                
+                    await interaction.followup.send(response, view=MyView(interaction, chatbot, conversation_style, suggest_responses), embeds=images_embed, wait=True)                
                 elif link_embed:
-                    await message.followup.send(response, view=MyView(chatbot, conversation_style, suggest_responses), embed=link_embed, wait=True)
+                    await interaction.followup.send(response, view=MyView(interaction, chatbot, conversation_style, suggest_responses), embed=link_embed, wait=True)
                 else:
-                    await message.followup.send(response, view=MyView(chatbot, conversation_style, suggest_responses), wait=True)
+                    await interaction.followup.send(response, view=MyView(interaction, chatbot, conversation_style, suggest_responses), wait=True)
             else:
                 if images_embed:
-                    await message.followup.send(response, embeds=images_embed, wait=True)
+                    await interaction.followup.send(response, embeds=images_embed, wait=True)
                 elif link_embed:
-                    await message.followup.send(response, embed=link_embed, wait=True)
+                    await interaction.followup.send(response, embed=link_embed, wait=True)
                 else:
-                    await message.followup.send(response, wait=True)
+                    await interaction.followup.send(response, wait=True)
         except Exception as e:
-                print(reply)
                 try:
                     if reply["item"]["throttling"]["numUserMessagesInConversation"] and reply["item"]["throttling"]["numUserMessagesInConversation"] > reply["item"]["throttling"]["maxNumUserMessagesInConversation"]:
-                        await message.followup.send("> **Oops, I think we've reached the end of this conversation. Please reset the bot!**")
+                        await interaction.followup.send("> **Oops, I think we've reached the end of this conversation. Please reset the bot!**")
                         logger.exception(f"Error while sending message: The maximum number of conversations in a round has been reached")
                 except:
-                    await message.followup.send("> **Error: Please try again later or reset bot**")
+                    await interaction.followup.send("> **Error: Please try again later or reset bot**")
                     logger.exception(f"Error while sending message: {e}")
+        finally:
+            using_func[interaction.user.id] = False

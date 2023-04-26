e040cb9149d3ab210bee064360895d0dfb6ca745
Danny <dd8611706@gmail.com>
2023-04-26 17:57:41 +0800

    feat: use app_commands module

--- cogs/imageGen.py
+++ cogs/imageGen.py
@@ -2,14 +2,13 @@ import discord
 import asyncio
 from src.imageCreate import create_image
 from src import log
-from discord.ext import commands
+from discord import app_commands
 from core.classes import Cog_Extension
 
-bot = commands.Bot(command_prefix='!', intents = discord.Intents.all())
 logger = log.setup_logger(__name__)
 
 class imageGen(Cog_Extension):
-    @bot.tree.command(name = "create_image", description = "generate image by bing image creator")
+    @app_commands.command(name = "create_image", description = "generate image by bing image creator")
     async def create_image(self, interaction: discord.Interaction, *, prompt: str):
         await interaction.response.defer(ephemeral=False, thinking=True)
         logger.info(f"\x1b[31m{interaction.user}\x1b[0m : '{prompt}' ({interaction.channel}) [BingImageCreator]")

1353f3f1dbfd48a6ab86e0e1445529e116ff17c4
FuseFairy <dd8611706@gmal.com>
2023-11-13 00:24:45 +0800

    modify command name

--- cogs/help.py
+++ cogs/help.py
@@ -5,10 +5,10 @@ from discord import app_commands
 class Help(Cog_Extension):
     @app_commands.command(name = "help", description = "Show how to use")
     async def help(self, interaction: discord.Interaction):
-        embed=discord.Embed(title="Help", description="[see more](https://github.com/FuseFairy/DiscordBot-EdgeGPT/blob/main/README.md)\n\n**COMMANDS -**")
-        embed.add_field(name="/bing_cookies", value="Set and delete your Bing Cookies.", inline=False)
+        embed=discord.Embed(description="[FuseFairy/DiscordBot-EdgeGPT](https://github.com/FuseFairy/DiscordBot-EdgeGPT/blob/main/README.md)\n***COMMANDS -***")
+        embed.add_field(name="/bing_setting", value="Set and delete your Bing Cookies.", inline=False)
         embed.add_field(name="/bing", value="Chat with Bing.", inline=False)
-        embed.add_field(name="/reset", value="Reset your Bing conversation.", inline=False)
+        embed.add_field(name="/reset_conversation", value="Reset your Bing conversation.", inline=False)
         embed.add_field(name="/switch_style", value="Switch your Bing conversation style.", inline=False)
         embed.add_field(name="/create_image", value="Generate image by Bing Image Creator.", inline=False)
         await interaction.response.send_message(embed=embed)

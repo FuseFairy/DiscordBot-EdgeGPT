cb8873117ff87926e32e31201d92bc23606d94c4
FuseFairy <dd8611706@gmal.com>
2023-11-13 00:23:25 +0800

    refactor: rewrite code using new bing api

--- cogs/event.py
+++ cogs/event.py
@@ -3,8 +3,8 @@ import re
 import os
 import json
 import asyncio
-from EdgeGPT.EdgeGPT import Chatbot, ConversationStyle
-from config import load_config
+from re_edge_gpt import Chatbot
+from re_edge_gpt import ConversationStyle
 from dotenv import load_dotenv
 from discord.ext import commands
 from core.classes import Cog_Extension
@@ -13,22 +13,21 @@ from src import log
 
 load_dotenv()
 
-USE_SUGGEST_RESPONSES = load_config.config["USE_SUGGEST_RESPONSES"]
 try:
     MENTION_CHANNEL_ID = int(os.getenv("MENTION_CHANNEL_ID"))
 except:
     MENTION_CHANNEL_ID = None
 logger = log.setup_logger(__name__)
 sem = asyncio.Semaphore(1)
-conversation_style = "balanced"
+conversation_style_str = "balanced"
 
 with open("./cookies.json", encoding="utf-8") as file:
     cookies = json.load(file)
 chatbot = Chatbot(cookies=cookies)
 
 # To add suggest responses
-class MyView(discord.ui.View):
-    def __init__(self, chatbot: Chatbot, suggest_responses:list):
+class ButtonView(discord.ui.View):
+    def __init__(self, suggest_responses:list):
         super().__init__(timeout=120)
         # Add buttons
         for label in suggest_responses:
@@ -43,11 +42,11 @@ class MyView(discord.ui.View):
                     username = str(interaction.user)
                     usermessage = button.label
                     channel = str(interaction.channel)
-                    logger.info(f"\x1b[31m{username}\x1b[0m : '{usermessage}' ({channel}) [Style: {conversation_style}] [button]")
-                    task = asyncio.create_task(send_message(chatbot, interaction, usermessage))
-                    await asyncio.gather(task)
+                    logger.info(f"\x1b[31m{username}\x1b[0m : '{usermessage}' ({channel}) [Style: {conversation_style_str}] [button]")
+                    await send_message(interaction, usermessage)
             self.add_item(button)
             self.children[-1].callback = partial(callback, button=button)
+
 # Show Dropdown
 class DropdownView(discord.ui.View):
     def __init__(self):
@@ -95,111 +94,90 @@ class DropdownView(discord.ui.View):
 
 # Set conversation style
 async def set_conversation_style(style: str):
-    global conversation_style
-    conversation_style = style
+    global conversation_style_str
+    conversation_style_str = style
+
 async def set_chatbot(cookies):
     global chatbot
     chatbot = Chatbot(cookies=cookies)
 
-async def send_message(chatbot: Chatbot, message, user_message: str):
+async def send_message(interaction, user_message: str):
     async with sem:
-        if isinstance(message, discord.message.Message):
-            await message.channel.typing()
         reply = ''
         text = ''
         link_embed = ''
-        images_embed = []
         all_url = []
+
+        if isinstance(interaction, discord.Interaction) and not interaction.response.is_done():
+            await interaction.response.defer(ephemeral=False, thinking=True)
+        elif isinstance(interaction, discord.message.Message):
+            await interaction.channel.typing()
+
         try:
-             # Change conversation style
-            if conversation_style == "creative":
-                reply = await chatbot.ask(prompt=user_message, conversation_style=ConversationStyle.creative, simplify_response=True)
-            elif conversation_style == "precise":
-                reply = await chatbot.ask(prompt=user_message, conversation_style=ConversationStyle.precise, simplify_response=True)
+            # Change conversation style
+            if conversation_style_str == "creative":
+                conversation_style=ConversationStyle.creative
+            elif conversation_style_str == "precise":
+                conversation_style=ConversationStyle.precise
             else:
-                reply = await chatbot.ask(prompt=user_message, conversation_style=ConversationStyle.balanced, simplify_response=True)
+                conversation_style=ConversationStyle.balanced
+
+            reply = await chatbot.ask(
+                prompt=user_message,
+                conversation_style=conversation_style,
+                simplify_response=True
+            )
 
             # Get reply text
             text = f"{reply['text']}"
             text = re.sub(r'\[\^(\d+)\^\]', lambda match: '', text)
-
+            
             # Get the URL, if available
             try:
-                if len(reply['sources']) != 0:
-                    for i, url in enumerate(reply['sources'], start=1):
-                        if len(url['providerDisplayName']) == 0:
-                            all_url.append(f"{i}. {url['seeMoreUrl']}")
-                        else:
-                            all_url.append(f"{i}. [{url['providerDisplayName']}]({url['seeMoreUrl']})")
+                if reply['sources_text']:
+                    urls = re.findall(r'\[(\d+)\. (.*?)\]\((https?://.*?)\)', reply["sources_text"])
+                    for url in urls:
+                        all_url.append(f"{url[0]}. [{url[1]}]({url[2]})")
                 link_text = "\n".join(all_url)
                 link_embed = discord.Embed(description=link_text)
             except:
                 pass
-
+            
             # Set the final message
-            if isinstance(message, discord.interactions.Interaction):
-                user_message = user_message.replace("\n", "")
-                ask = f"> **{user_message}**\t(***style: {conversation_style}***)\n\n"
-                response = f"{ask}{text}"
-            else:
-                response = f"{text}\t(***style: {conversation_style}***)"
-
+            user_message = user_message.replace("\n", "")
+            if isinstance(interaction, discord.Interaction):
+                ask = f"> **{user_message}** - <@{str(interaction.user.id)}> (***style: {conversation_style_str}***)\n\n"
+            elif isinstance(interaction, discord.message.Message):
+                ask = f"> **{user_message}**\t(***style: {conversation_style_str}***)\n\n"
+            response = f"{ask}{text}"
+            
             # Discord limit about 2000 characters for a message
             while len(response) > 2000:
                 temp = response[:2000]
                 response = response[2000:]
-                if isinstance(message, discord.interactions.Interaction): 
-                    await message.followup.send(temp)
-                else: 
-                    await message.channel.send(temp)
+                if isinstance(interaction, discord.Interaction):
+                    await interaction.followup.send(temp)
+                elif isinstance(interaction, discord.message.Message):
+                    await interaction.channel.send(temp)
+                
+            suggest_responses = reply["suggestions"]              
+            if link_embed:
+                if isinstance(interaction, discord.Interaction):
+                    await interaction.followup.send(response, view=ButtonView(suggest_responses), embed=link_embed, wait=True)
+                elif isinstance(interaction, discord.message.Message):
+                    await interaction.channel.send(response, view=ButtonView(suggest_responses), embed=link_embed)
+            else:
+                if isinstance(interaction, discord.Interaction):
+                    await interaction.followup.send(response, view=ButtonView(suggest_responses), wait=True)
+                elif isinstance(interaction, discord.message.Message):
+                    await interaction.channel.send(response, view=ButtonView(suggest_responses))
 
-            # Get the image, if available
-            try:
-                if len(link_embed) == 0:
-                    all_image = re.findall("https?://[\w\./]+", str(reply["sources_text"]))
-                    [images_embed.append(discord.Embed(url="https://www.bing.com/").set_image(url=image_link)) for image_link in all_image]
-            except:
-                pass
-            
-            if USE_SUGGEST_RESPONSES:
-                suggest_responses = reply["suggestions"]
-                if images_embed:
-                    if isinstance(message, discord.interactions.Interaction):
-                        await message.followup.send(response, view=MyView(chatbot, suggest_responses), embeds=images_embed, wait=True)                
-                    else:
-                        await message.channel.send(response, view=MyView(chatbot, suggest_responses), embeds=images_embed)
-                elif link_embed:
-                    if isinstance(message, discord.interactions.Interaction):
-                        await message.followup.send(response, view=MyView(chatbot, suggest_responses), embed=link_embed, wait=True)
-                    else:
-                        await message.channel.send(response, view=MyView(chatbot, suggest_responses), embed=link_embed)
-                else:
-                    if isinstance(message, discord.interactions.Interaction):
-                        await message.followup.send(response, view=MyView(chatbot, suggest_responses), wait=True)
-                    else:
-                        await message.channel.send(response, view=MyView(chatbot, suggest_responses))
-            else:
-                if images_embed:
-                    if isinstance(message, discord.interactions.Interaction):
-                        await message.followup.send(response, embeds=images_embed, wait=True)                
-                    else:
-                        await message.channel.send(response, embeds=images_embed)
-                elif link_embed:
-                    if isinstance(message, discord.interactions.Interaction):
-                        await message.followup.send(response, embed=link_embed, wait=True)
-                    else:
-                        await message.channel.send(response, embed=link_embed)
-                else:
-                    if isinstance(message, discord.interactions.Interaction):
-                        await message.followup.send(response, wait=True)
-                    else:
-                        await message.channel.send(response)
         except Exception as e:
-            if isinstance(message, discord.interactions.Interaction):
-                await message.followup.send(f">>> **Error: {e}**")
-            else:
-                await message.channel.send(f">>> **Error: {e}**")
-            logger.exception(f"Error while sending message: {e}")
+                if isinstance(interaction, discord.Interaction):
+                    await interaction.followup.send(f">>> **Error: {e}**")
+                elif isinstance(interaction, discord.message.Message):
+                    await interaction.channel.send(f">>> **Error: {e}**")
+                logger.error(f"Error while sending message: {e}")
 
 class Event(Cog_Extension):
     @commands.Cog.listener()
@@ -212,9 +190,8 @@ class Event(Cog_Extension):
                 if len(content) > 0:
                     username = str(message.author)
                     channel = str(message.channel)
-                    logger.info(f"\x1b[31m{username}\x1b[0m : '{content}' ({channel}) [Style: {conversation_style}]")
-                    task = asyncio.create_task(send_message(chatbot, message, content))
-                    await asyncio.gather(task)
+                    logger.info(f"\x1b[31m{username}\x1b[0m : '{content}' ({channel}) [Style: {conversation_style_str}]")
+                    await send_message(message, content)
                 else:
                     await message.channel.send(view=DropdownView())
             elif MENTION_CHANNEL_ID is not None:

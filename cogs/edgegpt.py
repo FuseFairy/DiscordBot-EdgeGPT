dae050f5d9d18dccb0f7a3b67049114e924f6ff0
FuseFairy <dd8611706@gmal.com>
2023-11-25 13:23:54 +0800

    refactor: Remove command name underline

--- cogs/edgegpt.py
+++ cogs/edgegpt.py
@@ -9,10 +9,23 @@ from src.user_chatbot import set_chatbot, get_users_chatbot, del_users_chatbot
 logger = log.setup_logger(__name__)
 
 class EdgeGPT(Cog_Extension):
+    chatbot_group = app_commands.Group(name="chatbot", description="Bing setting.")
+    switch_group = app_commands.Group(name="switch", description="Switch conversation style.")
+    create_group = app_commands.Group(name="create", description="Create images.")
+    reset_group = app_commands.Group(name="reset", description="Reset conversation.")
+
     # Set or delete Bing chatbot.
-    @app_commands.command(name="bing_setting", description="Set or delete bing chatbot.")
+    @chatbot_group.command(name="setting", description="Set or delete bing chatbot.")
     @app_commands.choices(choice=[app_commands.Choice(name="set", value="set"), app_commands.Choice(name="delete", value="delete")])
     async def cookies_setting(self, interaction: discord.Interaction, choice: app_commands.Choice[str], cookies_file: Optional[discord.Attachment]=None):
+        """Set or delete Bing chatbot.
+            
+            Parameters
+            -----------
+            choice: Choice[str]
+                Remember to upload your bing cookies if you choose to set it.
+        """
+
         await interaction.response.defer(ephemeral=True, thinking=True)
         user_id = interaction.user.id
         if choice.value == "set":
@@ -44,35 +57,35 @@ class EdgeGPT(Cog_Extension):
         user_id = interaction.user.id
         if user_id not in users_chatbot:
             await interaction.response.defer(ephemeral=True, thinking=True)
-            await interaction.followup.send("> **ERROE: Please use  ` /chatbot_setting`   to set yourself Bing chatbot first.**")
+            await interaction.followup.send("> **ERROE: Please use  ` /chatbot setting`   to set yourself Bing chatbot first.**")
         logger.info(f"{username}: {usermessage} ({channel}) [Style: {users_chatbot[user_id].get_conversation_style()}]")
         await users_chatbot[user_id].send_message(interaction, usermessage)
 
     # Switch conversation style.
-    @app_commands.command(name="switch_style", description="Switch conversation style")
+    @switch_group.command(name="style", description="Switch conversation style")
     @app_commands.choices(style=[app_commands.Choice(name="Creative", value="creative"), app_commands.Choice(name="Balanced", value="balanced"), app_commands.Choice(name="Precise", value="precise")])
     async def switch_style(self, interaction: discord.Interaction, style: app_commands.Choice[str]):
         await interaction.response.defer(ephemeral=True, thinking=True)
         users_chatbot = get_users_chatbot()
         user_id = interaction.user.id
         if user_id not in users_chatbot:
-            await interaction.followup.send("> **ERROR: Please use  ` /chatbot_setting`   to set yourself Bing chatbot first.**")
+            await interaction.followup.send("> **ERROR: Please use  ` /chatbot setting`   to set yourself Bing chatbot first.**")
         users_chatbot[user_id].set_conversation_style(style.value)
         await interaction.followup.send(f"> **INFO: successfull switch conversation style to {style.value}.**")
         
     # Create images.
-    @app_commands.command(name="create_image", description="generate image by Bing image creator")
+    @create_group.command(name="image", description="Generate image by Bing image creator")
     async def create_image(self, interaction: discord.Interaction, *, prompt: str):
         users_chatbot = get_users_chatbot()
         user_id = interaction.user.id
         if user_id not in users_chatbot:
             await interaction.response.defer(ephemeral=True, thinking=True)
-            await interaction.followup.send("> **ERROR: Please use  ` /chatbot_setting`   to set yourself Bing chatbot first.**")
+            await interaction.followup.send("> **ERROR: Please use  ` /chatbot setting`   to set yourself Bing chatbot first.**")
         await interaction.response.defer(ephemeral=False, thinking=True)
         await users_chatbot[user_id].create_image(interaction, prompt)
     
     # Reset conversation
-    @app_commands.command(name="reset_conversation", description="Reset bing chatbot conversation.")
+    @reset_group.command(name="conversation", description="Reset bing chatbot conversation.")
     async def reset_conversation(self, interaction: discord.Interaction):
         users_chatbot = get_users_chatbot()
         user_id = interaction.user.id

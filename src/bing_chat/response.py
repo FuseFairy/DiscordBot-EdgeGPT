f2856f1815c8690c9ae141e47ba66f9c9c79be4c
FuseFairy <dd8611706@gmal.com>
2024-01-14 16:00:43 +0800

    feat: response to thread

--- src/bing_chat/response.py
+++ src/bing_chat/response.py
@@ -7,14 +7,17 @@ from src.bing_chat.button_view import ButtonView
 
 logger = log.setup_logger(__name__)
 
-async def send_message(chatbot: Chatbot, interaction: discord.Interaction, user_message: str, image: str, conversation_style_str: str, users_chatbot=None, user_id=None):
+async def send_message(chatbot: Chatbot, user_message: str, image: str, conversation_style_str: str, users_chatbot=None, user_id=None, thread: discord.threads.Thread=None, interaction: discord.Interaction=None):
     reply = ''
     text = ''
     link_embed = ''
     all_url = []
     
-    if not interaction.response.is_done():
-        await interaction.response.defer(thinking=True)
+    if interaction:
+        if not interaction.response.is_done():
+            await interaction.response.defer(thinking=True)
+    else:
+        await thread.typing()
 
     try:
         # Change conversation style
@@ -46,8 +49,8 @@ async def send_message(chatbot: Chatbot, interaction: discord.Interaction, user_
         
         # Set the final message
         user_message = user_message.replace("\n", "")
-        ask = f"> **{user_message}** - <@{str(interaction.user.id)}> (***style: {conversation_style_str}***)\n\n"
-        response = f"{ask}{text}"
+        # ask = f"> **{user_message}** - <@{str(interaction.user.id)}> (***style: {conversation_style_str}***)\n\n"
+        response = f"{text} \n(***style: {conversation_style_str}***)"
         
         # Discord limit about 2000 characters for a message
         while len(response) > 2000:
@@ -55,11 +58,17 @@ async def send_message(chatbot: Chatbot, interaction: discord.Interaction, user_
             response = response[2000:]
             await interaction.followup.send(temp)
             
-        suggest_responses = reply["suggestions"]         
-        if link_embed:
-            await interaction.followup.send(response, view=ButtonView(interaction, conversation_style_str, suggest_responses, users_chatbot, user_id), embed=link_embed)
+        suggest_responses = reply["suggestions"]
+        if interaction:
+            if link_embed:
+                await interaction.followup.send(response, view=ButtonView(conversation_style_str, suggest_responses, users_chatbot, user_id), embed=link_embed)
+            else:
+                await interaction.followup.send(response, view=ButtonView(conversation_style_str, suggest_responses, users_chatbot, user_id))
         else:
-            await interaction.followup.send(response, view=ButtonView(interaction, conversation_style_str, suggest_responses, users_chatbot, user_id))
+            if link_embed:
+                await thread.send(content=response, view=ButtonView(conversation_style_str, suggest_responses, users_chatbot, user_id), embed=link_embed)
+            else:
+                await thread.send(content=response, view=ButtonView(conversation_style_str, suggest_responses, users_chatbot, user_id))
 
     except Exception as e:
         await interaction.followup.send(f"> **ERROR: {e}**")

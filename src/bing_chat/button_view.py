d341c59546c673a4bc73b73d7d8691958394ca97
FuseFairy <dd8611706@gmal.com>
2024-01-14 16:00:17 +0800

    feat: remove limit

--- src/bing_chat/button_view.py
+++ src/bing_chat/button_view.py
@@ -6,24 +6,19 @@ logger = log.setup_logger(__name__)
 
 # To add suggest responses
 class ButtonView(discord.ui.View):
-    def __init__(self, interaction: discord.Interaction, conversation_style_str:str, suggest_responses:list, users_chatbot, user_id):
+    def __init__(self, conversation_style_str:str, suggest_responses:list, users_chatbot, user_id):
         super().__init__(timeout=120)
-        self.button_author =interaction.user.id
 
         # Add buttons
         for label in suggest_responses:
             button = discord.ui.Button(label=label)
             # Button event
             async def callback(interaction: discord.Interaction, button: discord.ui.Button):     
-                if  interaction.user.id != self.button_author:
-                    await interaction.response.defer(ephemeral=True, thinking=True)
-                    await interaction.followup.send("You don't have permission to press this button.")
-                else:
-                    await interaction.response.defer(ephemeral=False, thinking=True)
-                    username = str(interaction.user)
-                    usermessage = button.label
-                    channel = str(interaction.channel)
-                    logger.info(f"\x1b[31m{username}\x1b[0m : '{usermessage}' ({channel}) [Style: {conversation_style_str}] [button]")
-                    await users_chatbot[user_id].send_message(interaction, usermessage)
+                await interaction.response.defer(ephemeral=False, thinking=True)
+                username = str(interaction.user)
+                usermessage = button.label
+                channel = str(interaction.channel)
+                logger.info(f"\x1b[31m{username}\x1b[0m : '{usermessage}' ({channel}) [Style: {conversation_style_str}] [button]")
+                await users_chatbot[user_id].send_message(message=usermessage, interaction=interaction)
             self.add_item(button)
             self.children[-1].callback = partial(callback, button=button)

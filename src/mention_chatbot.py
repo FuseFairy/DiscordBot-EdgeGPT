c084df8d72f59e211fb6199e9658f5cc1380d54b
FuseFairy <dd8611706@gmal.com>
2024-02-02 22:17:48 +0800

    first commit

---
+++ src/mention_chatbot.py
@@ -0,0 +1,34 @@
+import json
+import os
+from re_edge_gpt import Chatbot
+
+class MentionChatbot():
+    def __init__(self):
+        self.conversation_style = "balanced"
+        self.chatbot = None
+    
+    def set_conversation_style(self, conversation_style):
+        self.conversation_style = conversation_style
+
+    def get_conversation_style(self):
+        return self.conversation_style
+
+    async def set_chatbot(self, cookies: str=None):
+        try:
+            if cookies == None and os.path.isfile("./cookies.json"):
+                with open("./cookies.json", encoding="utf-8") as file:
+                    cookies = json.load(file)
+            self.chatbot = await Chatbot.create(cookies=cookies, mode="Copilot")
+        except:
+            return
+    
+    def get_chatbot(self):
+        return self.chatbot
+    
+    async def reset(self):
+        await self.chatbot.reset()
+
+client = MentionChatbot()
+
+def get_client():
+    return client

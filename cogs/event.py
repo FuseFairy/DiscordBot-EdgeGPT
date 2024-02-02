a87b5d7ba17ed8ad15e84865daecdc6a2df1e4e6
FuseFairy <dd8611706@gmal.com>
2024-02-02 22:28:50 +0800

    fix: conversation style is not work fine

--- cogs/event.py
+++ cogs/event.py
@@ -1,9 +1,7 @@
 import discord
 import re
 import os
-import json
 import asyncio
-from re_edge_gpt import Chatbot
 from re_edge_gpt import ConversationStyle
 from dotenv import load_dotenv
 from discord.ext import commands
@@ -11,6 +9,7 @@ from core.classes import Cog_Extension
 from functools import partial
 from src.log import setup_logger
 from src.user_chatbot import get_users_chatbot
+from src.mention_chatbot import get_client
 
 load_dotenv()
 
@@ -20,31 +19,25 @@ except:
     MENTION_CHANNEL_ID = None
 logger = setup_logger(__name__)
 sem = asyncio.Semaphore(1)
-conversation_style_str = "balanced"
-
-with open("./cookies.json", encoding="utf-8") as file:
-    cookies = json.load(file)
-chatbot = Chatbot(cookies=cookies)
 
 # To add suggest responses
 class ButtonView(discord.ui.View):
     def __init__(self, suggest_responses:list):
         super().__init__(timeout=120)
+        self.client = get_client()
+        self.conversation_style_str = self.client.get_conversation_style()
+        self.chatbot = self.client.get_chatbot()
         # Add buttons
         for label in suggest_responses:
             button = discord.ui.Button(label=label)
             # Button event
             async def callback(interaction: discord.Interaction, button: discord.ui.Button):
                     await interaction.response.defer(ephemeral=False, thinking=True)
-                    # When click the button, all buttons will disable.
-                    for child in self.children:
-                        child.disabled = True
-                    await interaction.followup.edit_message(message_id=interaction.message.id, view=self)
                     username = str(interaction.user)
                     usermessage = button.label
                     channel = str(interaction.channel)
-                    logger.info(f"\x1b[31m{username}\x1b[0m : '{usermessage}' ({channel}) [Style: {conversation_style_str}] [button]")
-                    await send_message(interaction, usermessage)
+                    logger.info(f"\x1b[31m{username}\x1b[0m : '{usermessage}' ({channel}) [Style: {self.conversation_style_str}] [button]")
+                    await send_message(interaction, self.chatbot, self.conversation_style_str, usermessage)
             self.add_item(button)
             self.children[-1].callback = partial(callback, button=button)
 
@@ -52,6 +45,7 @@ class ButtonView(discord.ui.View):
 class DropdownView(discord.ui.View):
     def __init__(self):
         super().__init__(timeout=180)
+        self.client = get_client()
 
         options = [
             discord.SelectOption(label="Creative", description="Switch conversation style to Creative", emoji='🎨'),
@@ -72,20 +66,21 @@ class DropdownView(discord.ui.View):
     # Dropdown event
     async def dropdown_callback(self, interaction: discord.Interaction):
         await interaction.response.defer(ephemeral=False, thinking=True)
+        await self.client.reset()
         if interaction.data['values'][0] == "Creative":
-            await set_conversation_style("creative")
+            self.client.set_conversation_style("creative")
             await interaction.followup.send(f"> **Info: successfull switch conversation style to *{interaction.data['values'][0]}*.**")
             logger.warning(f"\x1b[31mConversation style has been successfully switch to {interaction.data['values'][0]}\x1b[0m")
         elif interaction.data['values'][0] == "Balanced":
-            await set_conversation_style("balanced")
+            self.client.set_conversation_style("balanced")
             await interaction.followup.send(f"> **Info: successfull switch conversation style to *{interaction.data['values'][0]}*.**")
             logger.warning(f"\x1b[31mConversation style has been successfully switch to {interaction.data['values'][0]}\x1b[0m")
         elif interaction.data['values'][0] == "Precise":
-            await set_conversation_style("precise")
+            self.client.set_conversation_style("precise")
             await interaction.followup.send(f"> **Info: successfull switch conversation style to *{interaction.data['values'][0]}*.**")
             logger.warning(f"\x1b[31mConversation style has been successfully switch to {interaction.data['values'][0]}\x1b[0m")
         else:
-            await chatbot.reset()
+            await self.client.reset()
             await interaction.followup.send(f"> **Info: Reset finish.**")
             logger.warning("\x1b[31mBing has been successfully reset\x1b[0m")
         # disable dropdown after select
@@ -93,16 +88,7 @@ class DropdownView(discord.ui.View):
             dropdown.disabled = True
         await interaction.followup.edit_message(message_id=interaction.message.id, view=self)
 
-# Set conversation style
-async def set_conversation_style(style: str):
-    global conversation_style_str
-    conversation_style_str = style
-
-async def set_chatbot(cookies):
-    global chatbot
-    chatbot = Chatbot(cookies=cookies)
-
-async def send_message(interaction, user_message: str, image: str=None):
+async def send_message(interaction, chatbot, conversation_style_str, user_message: str, image: str=None):
     async with sem:
         reply = ''
         text = ''
@@ -130,7 +116,9 @@ async def send_message(interaction, user_message: str, image: str=None):
 
             # Get reply text
             text = f"{reply['text']}"
-            text = re.sub(r'\[\^(\d+)\^\]', lambda match: '', text)
+            text = re.sub(r'\[\^(\d+)\^\]',  '', text)
+            text = re.sub(r':\s*\[[^\]]*\]\([^\)]*\)', '', text)
+            text = re.sub(r'<[^>]*>', '', text)
             
             # Get the URL, if available
             urls = re.findall(r'\[(\d+)\. (.*?)\]\((https?://.*?)\)', reply["sources_link"])
@@ -141,7 +129,7 @@ async def send_message(interaction, user_message: str, image: str=None):
                 link_embed = discord.Embed(description=link_text)
             
             # Set the final message
-            response = f"{text} (***style: {conversation_style_str}***)"
+            response = f"{text} \n(***style: {conversation_style_str}***)"
             
             # Discord limit about 2000 characters for a message
             while len(response) > 2000:
@@ -165,11 +153,11 @@ async def send_message(interaction, user_message: str, image: str=None):
                     await interaction.channel.send(response, view=ButtonView(suggest_responses))
 
         except Exception as e:
-                if isinstance(interaction, discord.Interaction):
-                    await interaction.followup.send(f">>> **Error: {e}**")
-                elif isinstance(interaction, discord.message.Message):
-                    await interaction.channel.send(f">>> **Error: {e}**")
-                logger.error(f"Error: {e}")
+            if isinstance(interaction, discord.Interaction):
+                await interaction.followup.send(f">>> **Error：{e}**")
+            elif isinstance(interaction, discord.message.Message):
+                await interaction.channel.send(f">>> **Error：{e}**")
+            logger.error(f"Error：{e}")
 
 class Event(Cog_Extension):
     @commands.Cog.listener()
@@ -179,25 +167,28 @@ class Event(Cog_Extension):
         if self.bot.user in message.mentions:
             if not MENTION_CHANNEL_ID or message.channel.id == MENTION_CHANNEL_ID:
                 content = re.sub(r'<@.*?>', '', message.content).strip()
+                client = get_client()
+                conversation_style_str = client.get_conversation_style()
+                chatbot = client.get_chatbot()
                 if len(content) > 0:
                     username = str(message.author)
                     channel = str(message.channel)
                     if message.attachments:
                         for attachment in message.attachments:
                             if "image" in attachment.content_type:
-                                logger.info(f"\x1b[31m{username}\x1b[0m ： '{content}' ({channel}) [Style: {conversation_style_str}]")
+                                logger.info(f"\x1b[31m{username}\x1b[0m：'{content}' ({channel}) [Style: {conversation_style_str}]")
                                 async with message.channel.typing():
-                                    await send_message(message, content, attachment.url)
+                                    await send_message(message, chatbot, conversation_style_str, content, attachment.url)
                             else:
-                                await message.channel.send("> **ERROE: This file format is not supported.**")
+                                await message.channel.send("> **ERROE：This file format is not supported.**")
                     else:
-                        logger.info(f"\x1b[31m{username}\x1b[0m ： '{content}' ({channel}) [Style: {conversation_style_str}]")
+                        logger.info(f"\x1b[31m{username}\x1b[0m：'{content}' ({channel}) [Style: {conversation_style_str}]")
                         async with message.channel.typing():
-                            await send_message(message, content)
+                            await send_message(message, chatbot, conversation_style_str, content)
                 else:
                     await message.channel.send(view=DropdownView())
             elif MENTION_CHANNEL_ID is not None:
-                await message.channel.send(f"> **Can only be mentioned at <#{self.bot.get_channel(MENTION_CHANNEL_ID).id}>**")
+                await message.channel.send(f"> **Can only be mentioned at <#{MENTION_CHANNEL_ID}>**")
         else:
             try:
                 if isinstance(message.channel, discord.Thread):
@@ -209,18 +200,18 @@ class Event(Cog_Extension):
 
                     if user_thread != None and user_thread.id == message.channel.id:
                         content = message.content
-                        logger.info(f"\x1b[31m{username}\x1b[0m ： '{content}' ({channel}) [Style: {users_chatbot[user_id].get_conversation_style()}]")
+                        logger.info(f"\x1b[31m{username}\x1b[0m：'{content}' ({channel}) [Style: {users_chatbot[user_id].get_conversation_style()}]")
                         if message.attachments:
                             for attachment in message.attachments:
                                 if "image" in attachment.content_type:
                                     await users_chatbot[user_id].send_message(message=content, image=attachment.url)
                                 else:
-                                    await message.channel.send("> **ERROE: This file format is not supported.**")
+                                    await message.channel.send("> **ERROE：This file format is not supported.**")
                         else:
                             await users_chatbot[user_id].send_message(message=content)
             except Exception as e:
-                await message.channel.send(f"> **ERROR: {e}**")
-                logger.error(f"Error: {e}")   
+                await message.channel.send(f"> **ERROR：{e}**")
+                logger.error(f"Error：{e}")   
                     
                         
 async def setup(bot):

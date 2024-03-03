1952e138d15f0e7aa52bb9d63bcc21cdfc5e2600
FuseFairy <dd8611706@gmal.com>
2024-03-03 22:55:49 +0800

    refactor: Change chatbot mode from Copilot to Bing

--- src/mention_chatbot.py
+++ src/mention_chatbot.py
@@ -18,7 +18,7 @@ class MentionChatbot():
             if cookies == None and os.path.isfile("./cookies.json"):
                 with open("./cookies.json", encoding="utf-8") as file:
                     cookies = json.load(file)
-            self.chatbot = await Chatbot.create(cookies=cookies, mode="Copilot")
+            self.chatbot = await Chatbot.create(cookies=cookies, mode="Bing")
         except:
             return
     

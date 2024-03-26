2e18188c3254ddf251902e1181451ad01aadebcd
FuseFairy <dd8611706@gmail.com>
2024-03-26 13:47:01 +0800

    refactor: shorten ButtonView class by passing chatbot instance directly to create_image method

--- src/image/button_view.py
+++ src/image/button_view.py
@@ -1,20 +1,19 @@
 import discord
 
 class ButtonView(discord.ui.View):
-    def __init__(self, interaction: discord.Interaction, prompt: str, images: list, users_chatbot: dict, user_id: int):
+    def __init__(self, interaction: discord.Interaction, prompt: str, chatbot, images: list=None):
         super().__init__(timeout=180)
         self.button_author = interaction.user.id
         self.prompt = prompt
-        self.users_chatbot = users_chatbot
-        self.user_id = user_id
+        self.chatbot = chatbot
 
-        for i, image in enumerate(images, start=1):
-            if i > 2:
-                self.add_item(discord.ui.Button(label=f"Link {i}", url=image, row=2))
-            else:
-                self.add_item(discord.ui.Button(label=f"Link {i}", url=image, row=1))
+        if images:
+            for i, image in enumerate(images, start=1):
+                if i > 2:
+                    self.add_item(discord.ui.Button(label=f"Link {i}", url=image, row=2))
+                else:
+                    self.add_item(discord.ui.Button(label=f"Link {i}", url=image, row=1))
             
-
     # Button event
     @discord.ui.button(label="Regenerate", emoji="🔂", row=3, style=discord.ButtonStyle.blurple)
     async def callback(self,interaction: discord.Interaction, button: discord.ui.Button):
@@ -23,5 +22,4 @@ class ButtonView(discord.ui.View):
             await interaction.followup.send("You don't have permission to press this button.")
         else:
             await interaction.response.defer(ephemeral=False, thinking=True)
-            await self.users_chatbot[self.user_id].create_image(interaction, self.prompt)
-
+            await self.chatbot.create_image(interaction, self.prompt)

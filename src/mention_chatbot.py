61a1ce081b713b832d5dd07e9ce9cb4dc4fe7f2f
FuseFairy <dd8611706@gmail.com>
2024-05-22 18:22:51 +0800

    refactor: add refresh_cookies

--- src/mention_chatbot.py
+++ src/mention_chatbot.py
@@ -1,8 +1,12 @@
 import json
 import os
+import asyncio
 from .log import setup_logger
 from re_edge_gpt import Chatbot
+from dotenv import load_dotenv
+from src.auto_cookies import refresh_cookies
 
+load_dotenv()
 logger = setup_logger(__name__)
 
 class MentionChatbot():
@@ -18,15 +22,19 @@ class MentionChatbot():
 
     async def set_chatbot(self, cookies: str=None):
         try:
-            if not os.path.exists("./cookies.json") and not os.getenv("BING_COOKIES"):
+            if os.getenv("AUTO_COOKIES"):       
+                asyncio.create_task(refresh_cookies())
+            elif not os.path.exists("./cookies.json") and not os.getenv("BING_COOKIES"):
                 logger.error("Please setup your Bing cookies.")
                 return
+            
             if cookies == None:
                 if os.getenv("BING_COOKIES"):
                     cookies = json.loads(os.getenv("BING_COOKIES"))
                 elif os.path.exists("./cookies.json"):
                     with open("./cookies.json", encoding="utf-8") as file:
                         cookies = json.load(file)
+
             self.chatbot = await Chatbot.create(proxy=os.getenv("PROXY"), cookies=cookies, mode="Bing")
         except Exception as e:
             logger.error(e, exc_info=True)
